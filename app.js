6b708aad3f2329f0ce295d238194db9acdc8db71
vikrantpahwa <vikrantpahwa0@gmail.com>
2021-12-27 06:12:31 -0800

    Working Project Withour Database

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 const express = require('express');
 
 bodyParser=require("body-parser");
-
+const date=require(__dirname+"/date.js")
 const app=express();
-
 app.set('view engine','ejs');
 
-var items=[];
+let items=[];
+let workItems=[];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.get("/",function(request,response){
-    
-var today=new Date();
+response.render("list",{listTitle:date.getDate(),newListItems:items});
+})
 
-var options={
-    weekday:"long",
-    day:"numeric",
-    month:"long"
-};
-var day=today.toLocaleDateString("en-US",options);
-response.render("list",{kindOfDay:day,newListItems:items});
+app.get("/work",function(req,res){
+
+    res.render("list",{listTitle:"WorkList",newListItems:workItems});
+
 })
 
 app.post("/",function(req,res){
 
-var item=req.body.newItem;
+let item=req.body.newItem;
+
+if(req.body.list==="WorkList"){
+    workItems.push(item);
+    res.redirect("/work");
+}
+else{    
 items.push(item);
 res.redirect("/");
-
+}
 })
 
 app.listen(2000,function(){
